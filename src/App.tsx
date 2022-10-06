2065b56e655fc08c679006ea259231b2c944da70
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-10-06 22:12:55 +0200

    finsing navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {Route,Routes} from 'react-router-dom';
+import  Box  from '@mui/material/Box';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
 import './App.css'
-
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 function App() {
 
   return (
-    <div className="App">
-    </div>
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
